983955e8460042c03125a79144f4d386b2c76c7f
nazar <nhavronskyi@gmail.com>
2023-07-10 21:55:56 +0200

    simple connection to config

---
+++ src/main/java/com/example/spotikservice/SpotikServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotikservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotikServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotikServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spotikservice/SpotikServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotikservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotikServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
