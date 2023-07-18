902f38c187998353a383be4263ae7cec12068d2a
nazar <nhavronskyi@gmail.com>
2023-07-18 15:12:40 +0200

    CallbackController

--- src/main/java/com/example/spotikservice/controller/CallbackController.java
+++ src/main/java/com/example/spotikservice/controller/CallbackController.java
@@ -7,24 +7,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import se.michaelthelin.spotify.SpotifyApi;
-import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
 import se.michaelthelin.spotify.model_objects.specification.PlaylistSimplified;
 
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
 public class CallbackController {
-
     private final SpotifyApi spotifyApi;
 
     @SneakyThrows
     @GetMapping
-    public PlaylistSimplified[] getAllPlaylists(@RequestParam String code){
-        AuthorizationCodeCredentials execute = spotifyApi.authorizationCode(code)
+    public void setAccessToken(@RequestParam String code) {
+        var execute = spotifyApi.authorizationCode(code)
                 .build().execute();
+        spotifyApi.setAccessToken(execute.getAccessToken());
+    }
 
-        String accessToken = execute.getAccessToken();
-        spotifyApi.setAccessToken(accessToken);
+    @SneakyThrows
+    @GetMapping("playlists")
+    public PlaylistSimplified[] getPlaylists() {
         return spotifyApi.getListOfCurrentUsersPlaylists()
                 .build().execute().getItems();
     }
