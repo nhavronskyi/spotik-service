83357b8967c3b903c57c575d66306e44b6e65f41
Oksana Lahutina <oksankalahutina@gmail.com>
2023-07-30 00:12:03 +0300

    Deleted column annotation(not obligatory anymore)

--- src/main/java/com/example/spotikservice/entities/User.java
+++ src/main/java/com/example/spotikservice/entities/User.java
@@ -1,7 +1,5 @@
 package com.example.spotikservice.entities;
 
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -20,12 +18,9 @@ public class User {
     @Id
     private String id;
 
-    @Column(name = "access_token")
     private String accessToken;
 
-    @Column(name = "refresh_token")
     private String refreshToken;
 
-    @Column(name = "access_token_expiration_time")
     private long accessTokenExpirationTime;
 }
