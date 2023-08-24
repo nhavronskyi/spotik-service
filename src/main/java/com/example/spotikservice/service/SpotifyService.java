d1f9d79806b3cb9b18bcd7e1a70d7abd88ed361e
nazar <nhavronskyi@gmail.com>
2023-08-24 22:40:09 +0200

    resolved problems

---
+++ src/main/java/com/example/spotikservice/service/SpotifyService.java
@@ -0,0 +1,27 @@
+package com.example.spotikservice.service;
+
+import se.michaelthelin.spotify.model_objects.specification.*;
+
+import java.util.List;
+import java.util.TreeMap;
+public interface SpotifyService {
+    String getAccountEmail();
+
+    List<PlaylistSimplified> getPlaylists();
+
+    List<Album> getAlbums();
+
+    List<Track> getSavedSongs();
+
+    List<TrackSimplified> getTracksFromAlbumByCountry(String albumId, String code);
+
+    TreeMap<String, List<AlbumSimplified>> getLastReleasesFromSubscribedArtists();
+
+    List<PlaylistTrack> getTracksFromPlaylistByCountry(String id, String code);
+
+    List<Track> getTracksFromAccountByCountry(String code);
+
+    void removeAllTracksFromPlaylistByCountry(String playlistId, String code);
+
+    void removeTrackFromPlaylist(String playlistId, String trackId);
+}
